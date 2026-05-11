750e596000028a305b60e835b94048ce59ed232b
Certefied-dumbass <24399lc@hvhs.school.nz>
2026-05-11 23:56:21 +0000

    May 11, 2026, 11:56 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -42,7 +42,7 @@ function getFormOutput(){
 function getFormpay(){
   const _hours = document.getElementById("userpay").value;
   _hours = PAY_FIELD;
-console.log}
+console.log (_hours) }
 writeline()
 // imag()
 welcome("loser", 15)
